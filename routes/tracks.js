6ac767ffb0489b7c5d8e04a78180df750542317b
Yaroslav Grogul <time7bass@gmail.com>
2022-12-19 14:24:47 +0100

    images links were added to tracks endpoint

--- routes/tracks.js
+++ routes/tracks.js
@@ -7,9 +7,51 @@ const dbConfig = require('../config/connection-config');
 const connection = mysql.createConnection(dbConfig)
 
 
+async function fetchImageUrl(url) {
+  // const [imageUrl, setImageUrl] = useState(null);
+  // console.log("Printing the IMGUrl for ", url)
+
+  var result = url.split('/')
+  result = result[result.length - 1]
+  var fetchingUrl = "https://en.wikipedia.org/w/api.php?action=query&titles="+result+"&prop=pageimages&format=json&pithumbsize=500&redirects&origin=*"
+
+  var imgUrl = ""
+  return(
+  fetch(fetchingUrl, {
+      mode: 'cors', // Set the mode to 'no-cors' to fetch the data without CORS
+      
+  })
+      .then(response => response.text())
+      // .then(result => console.log("Result: ", result))
+      .then(function(data) {
+          data = data.split('source')[1]
+          data = data.substring(3)
+          // console.log("Data", data.split("\",\"width")[0])
+          data = data.split("\",\"width")[0]
+          // console.log("Data: ", data)
+          return(data)
+          // console.log("FetchingUrl is ", fetchingUrl)
+          // console.log(data)
+          // if (data.startsWith("https://")) {
+          //   // console.log("Data, ", data)
+          //       data
+          // } else {
+          //   // console.log("ImgUrl Not found! for ", url)
+          // }
+      })
+      // .then(data => console.log("Data2: ", data))
+
+  )
+      
+      // .catch(error => {
+      //   // console.error(error);
+      //   Error(error)
+      // });
+}
+
 
 const getTracks = (request, response) => {
-  console.log(request.query)
+  // console.log(request.query)
   if (Object.keys(request.query).length == 1) {
     var query = 'SELECT * FROM tCircuits where ';
 
@@ -25,16 +67,54 @@ const getTracks = (request, response) => {
 
     })
   } else {
-    console.log("No params :(")
+    // console.log("No params :(")
     connection.query('SELECT * FROM tCircuits', (error, results) => {
       if (error) {
         throw error
       }
-      response.send(results)
+      // response.send(results)
+      // results = results[0]
+
+      fetches=[];
+
+      resultsWithImg = [];
+
+      results.forEach(track => {
+        fetches.push(
+          fetchImageUrl(track.url)
+          .then(result => {
+
+            // console.log("Result: ", result)
+            if (result.startsWith("https://")) {
+              track.imgurl = result
+            } else {
+              track.imgurl = "no-data"
+            }
+
+            resultsWithImg.push(track)
+
+            if (resultsWithImg.length == results.length) {
+              response.send(resultsWithImg)
+            }
+
+            
+          })
+          .catch(
+            // console.log("Error: ", error)
+          )
+        )
+
+      });
+        // .then(console.log(fetches))
+
+
+
+      // response.send(results)
 
     })
   }  
 }
+
 
 /* GET tracks page. */
 router.get('/', getTracks);
